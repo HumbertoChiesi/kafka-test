6e5327db42c0e5b12cfe40becc262e658706b474
HumbertoChiesi <66853228+HumbertoChiesi@users.noreply.github.com>
2024-03-12 11:05:09 -0300

    change to json data

--- kafka-test.py
+++ kafka-test.py
@@ -1,23 +1,24 @@
 from kafka import KafkaProducer, KafkaConsumer
+import time
+import json
 
-bootstrap_servers = 'localhost:9092'
+bootstrap_servers = 'localhost:49092,localhost:39092,localhost:29092'
 topic = 'test_topic'
 
 
-def produce_message(producer, topic):
-    # Produce a test message
-    message = b"Hello, Kafka!"
+def produce_message(producer, topic, json_data):
+    message = json.dumps(json_data).encode('utf-8')
     producer.send(topic, value=message)
     producer.flush()
     print("Message sent successfully.")
+    consume_messages(consumer, topic)
 
 
 def consume_messages(consumer, topic):
-    # Subscribe to the topic
     consumer.subscribe(topics=[topic])
-    # Consume messages
+
     for message in consumer:
-        print("Received message:", message.value.decode('utf-8'))
+        print("Received message:", json.loads(message.value.decode('utf-8')))
 
 
 if __name__ == "__main__":
@@ -27,8 +28,6 @@ if __name__ == "__main__":
                              enable_auto_commit=True,
                              group_id='test_group')
 
-    # Produce a test message
-    produce_message(producer, topic)
+    data = {"time": time.time()}
 
-    # Consume messages
-    consume_messages(consumer, topic)
+    produce_message(producer, topic, data)
